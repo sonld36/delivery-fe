81713c05e727e2e516ed5e4edab603e33d6ef4be
sonld <sonld@sapo.vn>
2023-08-09 04:46:16 +0700

    fix final

--- src/services/http-common.ts
+++ src/services/http-common.ts
@@ -3,7 +3,7 @@ import { store } from "@App/store";
 import axios from "axios";
 // https://delivery-system.onrender.com
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://delivery-system.onrender.com/api/",
   headers: {
     "Content-type": "application/json",
   },
